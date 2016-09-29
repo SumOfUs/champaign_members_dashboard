ef0aaa3697a843f46730eef4bbd12c4960c62cf2
Vincent Martinez <vincent@strict.so>
2016-09-29 18:48:09 +0100

    Use react-scripts for devServer and production build

--- config/development.example.js
+++
@@ -1,7 +0,0 @@
-// For each environment, you need a file named `config/${environment}.js` that
-// will be loaded whenever the app imports the 'app-config' module.
-// Webpack has been configured to alias 'app-config' => 'config/{ENV}.js'
-export default {
-  apiUrl: process.env.API_URL || 'http://localhost:3000',
-  debug: true,
-};
