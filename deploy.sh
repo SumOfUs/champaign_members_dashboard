b454d3f5b6d8800fe39c6d3a4c96aa5575eb2930
Omar Sahyoun <osahyoun@gmail.com>
2016-10-06 16:48:00 +0100

    CI related files

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+SHA1=$1
+
+docker push osahyoun/members_dashboard:$SHA1
+
+EB_BUCKET=members-dashboard-deployment
+DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+aws elasticbeanstalk create-application-version --application-name members-dashabord \
+  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+
+aws elasticbeanstalk update-environment --environment-name members-dashboard-env-production \
+    --version-label $SHA1
