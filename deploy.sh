6b0a990cb25eda641898d2d2f33a6abb4ea8634f
Omar Sahyoun <osahyoun@gmail.com>
2016-10-19 13:55:19 +0100

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ aws configure set default.region $AWS_REGION
 
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name members-dashabord \
+aws elasticbeanstalk create-application-version --application-name members-dashboard \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 aws elasticbeanstalk update-environment --environment-name members-dashboard-env-production \
