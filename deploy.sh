96b36264acefee83fafae4a065793676b72b5ea1
tuuli <tuuli.pollanen@gmail.com>
2016-10-19 13:51:59 -0700

    staging

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,23 @@
 #! /bin/bash
 
 SHA1=$1
+EB_BUCKET=$2
+ENVIRONMENT_NAME=$3
+export URL=$4
 
 docker push soutech/members_dashboard:$SHA1
 
-EB_BUCKET=members-dashboard-deployment
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 aws configure set default.region $AWS_REGION
 
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+
+cat config/nginx.conf | envsubst '$URL' >tmp
+mv tmp config/nginx.conf
+
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+
 aws elasticbeanstalk create-application-version --application-name members-dashboard \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
-aws elasticbeanstalk update-environment --environment-name members-dashboard-env-production \
-    --version-label $SHA1
+aws elasticbeanstalk update-environment --environment-name $ENVIRONMENT_NAME --version-label $SHA1
