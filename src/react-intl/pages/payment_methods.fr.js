236122dae0c9c69a1479f786c953b439b6681c61
Paul Ferris <paul@sumofus.org>
2016-12-05 14:22:02 +0100

    Update payment_methods.fr.js

--- src/react-intl/pages/payment_methods.fr.js
+++ src/react-intl/pages/payment_methods.fr.js
@@ -1,9 +1,2 @@
 export const description = `
-  Thanks for your supporting SumOfUs! Here, you can view and delete
-  any payment methods you've saved with SumOfUs to make donating
-  easier and more secure. To add a new payment method,
-  just [make a donation to SumOfUs](https://actions.sumofus.org/a/donate) and make sure
-  you've opted to store your payment details. You can always be in touch
-  at [donations@sumofus.org](mailto:donations@sumofus.org) with
-  any questions!`;
-
+  Merci de soutenir SumOfUs ! Ici, vous pouvez voir et effacer les moyens de paiement que vous avez sauvegardé avec SumOfUs afin de simplifier et de sécuriser vos dons. Pour ajouter un moyen de paiement, [faites tout simplement un don à SumOfUs](https://actions.sumofus.org/a/donate) et assurez vous de choisir la sauvegarde de votre moyen de paiement. Vous pouvez également nous contacter à [donations@sumofus.org](mailto:donations@sumofus.org) si vous avez une question.`;
