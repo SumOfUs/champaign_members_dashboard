365c102c55810a188f5bbf9e0383ca1e2a84f84c
Subbiah <49471498+subbiahsn@users.noreply.github.com>
2023-03-16 12:35:45 +0530

    Make commands executable

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -eu -o pipefail
 
-docker build --rm=false -t  soutech/members_dashboard:$CIRCLE_SHA1 .
+SHA1=$1
+
+docker build --rm=false -t  soutech/members_dashboard:$SHA1 .
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker push soutech/members_dashboard:$CIRCLE_SHA1
+docker push soutech/members_dashboard:$SHA1
