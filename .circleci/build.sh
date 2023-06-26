317bdde754ac9a5e418356b6690aa83606091fd0
Yesi Molina <yesi@yesimolina.dev>
2023-06-26 15:33:09 -0600

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -3,6 +3,6 @@
 SHA1=$1
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 
-docker build --rm=false -t  soutech/members_dashboard:$SHA1 .
 docker tag soutech/members_dashboard:$SHA1 soutech/members_dashboard:$SHA1
+docker build --rm=false -t  soutech/members_dashboard:$SHA1 .
 docker push soutech/members_dashboard:$SHA1
